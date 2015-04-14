ddee87eb3e7286e5bb8037841e2c04184c3c7b42
Tim Ermilov <tim.ermilov@eccenca.com>
2015-04-14 11:12:41 +0200

    split into more separate files, fix compile errors

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -3,7 +3,7 @@ var gulp = require('gulp');
 var less = require('gulp-less');
 
 module.exports = function() {
-    return gulp.src('./src/*.less')
+    return gulp.src('./src/style.less')
         .pipe(less())
         .pipe(gulp.dest('./css'));
 };
