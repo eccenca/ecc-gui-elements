693c5d1ca2e93a2165de85f43bff287e8a679b61
Michael Haschke <mhaschke@brox.de>
2015-11-04 17:17:57 +0100

    [mod] update Spinner element to inline, local and global usage via attribute, currently global is default

--- src/Spinner.js
+++ src/Spinner.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {MaterialMixin} from 'ecc-mixins';
+import classNames from 'classnames';
 import ReactMDLSpinner from 'react-mdl/lib/Spinner';
 
 const Spinner = React.createClass({
@@ -7,13 +8,31 @@ const Spinner = React.createClass({
 
     // define property types
     propTypes: {
+        appearGlobal: React.PropTypes.bool,
+        appearInline: React.PropTypes.bool,
+        appearLocal: React.PropTypes.bool,
         className: React.PropTypes.string,
+    },
+    getDefaultProps() {
+        return {
+            appearGlobal: true,
+            appearInline: false,
+            appearLocal: false,
+        };
     },
 
     // template rendering
     render() {
-        const {className, ...otherProps} = this.props;
-        return <ReactMDLSpinner singleColor={true} className={className} {...otherProps} />;
+        const {className, appearGlobal, appearInline, appearLocal, ...otherProps} = this.props;
+        const classes = classNames(
+            {
+                'mdl-spinner--global': (appearGlobal === true && appearInline === false && appearLocal === false),
+                'mdl-spinner--inline': (appearInline === true),
+                'mdl-spinner--local': appearLocal === true,
+            },
+            className
+        );
+        return <ReactMDLSpinner singleColor={true} className={classes} {...otherProps} />;
     },
 });
 
