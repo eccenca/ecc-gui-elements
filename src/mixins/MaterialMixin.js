2297f96d249992e9c759b14fc81a7b7919129489
Lukas Eipert <git@leipert.io>
2016-01-13 20:43:24 +0100

    improves MaterialMixin

--- src/mixins/MaterialMixin.js
+++ src/mixins/MaterialMixin.js
@@ -1,11 +1,17 @@
 import materialDesign from 'material-design-lite/compiled/material';
+import { findDOMNode } from 'react-dom';
 
 const MaterialMixin = {
     materialDesign,
-
-    componentDidUpdate() {
+    componentDidMount() {
         window.componentHandler.upgradeDom();
     },
+    componentWillUnmount() {
+        window.componentHandler.downgradeElements(findDOMNode(this));
+    },
+    componentDidUpdate() {
+        window.componentHandler.upgradeElements(findDOMNode(this));
+    }
 };
 
 export default MaterialMixin;
