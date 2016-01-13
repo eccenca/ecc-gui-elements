df91a25ee6815ebb4a89f35a7ef9c12e0ddc132c
Lukas Eipert <git@leipert.io>
2016-01-13 23:38:52 +0100

    fixes further problems with the mixins

--- src/mixins/MaterialMixin.js
+++ src/mixins/MaterialMixin.js
@@ -1,16 +1,17 @@
+/*global componentHandler */
 import materialDesign from 'material-design-lite/compiled/material';
-import { findDOMNode } from 'react-dom';
+import {findDOMNode} from 'react-dom';
 
 const MaterialMixin = {
     materialDesign,
     componentDidMount() {
-        window.componentHandler.upgradeDom();
+        componentHandler.upgradeDom();
     },
     componentWillUnmount() {
-        window.componentHandler.downgradeElements(findDOMNode(this));
+        componentHandler.downgradeElements(findDOMNode(this));
     },
     componentDidUpdate() {
-        window.componentHandler.upgradeElements(findDOMNode(this));
+        componentHandler.upgradeElements(findDOMNode(this));
     }
 };
 
