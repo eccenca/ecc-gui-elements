310e1b57cb219acb5ff76f7d66bd090aee1068e1
Lukas Eipert <lukas.eipert@eccenca.com>
2016-01-22 16:09:05 +0100

    created gulp task which resolves files in node_modules correctly

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp');
+
+gulp.task('default', function () {
+    var path = require('path');
+    var sass = require('gulp-sass');
+
+    var nodeModules = path.join(process.cwd(), 'node_modules')
+
+    return gulp.src('./src/main.node.scss')
+        .pipe(sass({
+            importer : function(url, prev, done){
+
+                if(url.indexOf('~') === 0){
+
+                    url = path.join(nodeModules, url.substr(1));
+                    console.log('Resolved file to: ' + url);
+
+                }
+
+                done({file: url})
+
+            }
+        }).on('error', sass.logError))
+        .pipe(gulp.dest('./css'));
+});
