a8510a67b4266f525009a30e44ab468b63145eb3
Michael Haschke <michael.haschke@eccenca.com>
2016-02-16 17:42:49 +0100

    generate json file that holds table of icons and their names

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 var gulp = require('gulp');
 
-gulp.task('default', function () {
+gulp.task('sass', function () {
     var path = require('path');
     var sass = require('gulp-sass');
     var rename = require("gulp-rename");
@@ -25,3 +25,44 @@ gulp.task('default', function () {
         .pipe(rename("main.css"))
         .pipe(gulp.dest('./dist'));
 });
+
+gulp.task('icontable', function () {
+
+    //var csv = require('gulp-csvtojson');
+    var rename = require('gulp-rename');
+
+    /* TODO:
+       Using insert and replace as workaround as long as gulp-csvtojson doesn't
+       use recent csvtojson version that offers more options and callbacks.
+    */
+    var insert = require('gulp-insert');
+    var replace = require('gulp-replace');
+
+    return gulp.src('./dist/fonts/materialicons/codepoints')
+        .pipe(
+            replace(' ', '":"&#x')
+        )
+        .pipe(
+            replace('\n', '","')
+        )
+        .pipe(
+            insert.wrap('{"','"}')
+        )
+        .pipe(
+            replace(',""}', '}')
+        )
+        /*
+        .pipe(csv({
+            constructResult: false,
+            checkType: false,
+            delimiter: ' ',
+            noheader: true,
+            headers:['ligatur','charcode'],
+            trim: true,
+        }))
+        */
+        .pipe(rename("icontable.json"))
+        .pipe(gulp.dest('./dist/fonts/materialicons'));
+});
+
+gulp.task('default', ['sass', 'icontable']);
