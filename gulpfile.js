8017369988e8e2a9dd1090d7258c437d9df84371
Lukas Eipert <lukas.eipert@eccenca.com>
2017-01-11 22:01:19 +0100

    update build tools a bit

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,53 @@
 var gulp = require('ecc-gulp-tasks')(require('./buildConfig.js'));
 var gulpSequence = require('gulp-sequence');
 
+var fs = require('fs');
+var path = require('path');
+var _ = require('lodash');
+
+
+var visp = require('./lib/vis/package.json');
+
 var iconFontVersion = '3.0.1';
 var robotoFontVersion = 'v1.1.0';
+var visVersion = visp.version;
 
 gulp.task('default', ['debug']);
 
-gulp.task('full-build', gulpSequence(['vis', 'build-sass'], 'build'));
+gulp.task('full-build', gulpSequence(['vis', 'build-sass'], 'update-licenses', 'build'));
 
 gulp.task('full-test', ['test', 'sass-compile']);
 
 gulp.task('build-sass', ['sass-compile', 'sass-assets']);
 
 gulp.task('download-fonts', ['download-iconfont', 'download-roboto', 'icontable']);
+
+gulp.task('update-licenses', function(cb) {
+    const yaml = require('js-yaml');
+
+    const licenseFile = path.join(__dirname, 'additionalLicenses.yml');
+
+    try {
+        let doc = fs.readFileSync(licenseFile, 'utf8');
+
+        dependencies = _.forEach(yaml.safeLoad(doc)['ecc-gui-elements']['dependencies'], (dependency) => {
+
+            const oldVersion = new RegExp(_.toString(dependency.version).replace(/\./g, '\\.'), 'g');
+
+            if (dependency.name === 'vis') {
+                doc = doc.replace(oldVersion, visVersion)
+            } else if (dependency.name === 'material-design-icons') {
+                doc = doc.replace(oldVersion, iconFontVersion)
+            }
+
+        });
+
+        fs.writeFile(licenseFile, doc, cb)
+
+    } catch (e) {
+        cb(e);
+    }
+});
 
 gulp.task('download-iconfont', function() {
     var download = require('gulp-download-stream');
@@ -67,9 +102,9 @@ gulp.task('download-roboto', function() {
         `https://github.com/FontFaceKit/roboto/raw/${robotoFontVersion}/fonts/ThinItalic/Roboto-ThinItalic.woff`,
         `https://github.com/FontFaceKit/roboto/raw/${robotoFontVersion}/fonts/ThinItalic/Roboto-ThinItalic.woff2`,
     ])
-        .pipe(rename(function(path){
-            path.dirname += '/'
-            path.dirname += path.basename.replace(/Roboto-/,'')
+        .pipe(rename(function(paths) {
+            paths.dirname += '/'
+            paths.dirname += paths.basename.replace(/Roboto-/, '')
         }))
         .pipe(gulp.dest('./dist/fonts/roboto'));
 });
@@ -80,8 +115,6 @@ gulp.task('sass-assets', function() {
 });
 
 gulp.task('sass-compile', function(cb) {
-    var fs = require('fs');
-    var path = require('path');
     var sass = require('node-sass');
     sass.render({
         file: './style/core/main.build-css.scss',
@@ -103,10 +136,8 @@ gulp.task('sass-compile', function(cb) {
     });
 });
 
+
 gulp.task('vis', function(cb) {
-    var _ = require('lodash');
-    var fs = require('fs');
-    var visp = require('./lib/vis/package.json');
     var p = require('./package.json');
 
     console.log('The vis dependency is not managed normally, but as a submodule.')
@@ -120,10 +151,6 @@ gulp.task('vis', function(cb) {
 
         if (_.includes(_.keys(visp.dependencies), key)) {
             return visp.dependencies[key];
-        }
-
-        if (key === 'vis') {
-            return visp.version;
         }
 
         return value;
@@ -142,8 +169,6 @@ gulp.task('download-codepoints', function() {
 });
 
 gulp.task('icontable', ['download-codepoints'], function(cb) {
-
-    var fs = require('fs');
 
     fs.readFile('./dist/fonts/iconfont/codepoints', 'utf8', function(err, data) {
 
