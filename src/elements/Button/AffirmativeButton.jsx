73274b88fa7850dcef96c714ebba2d3f899155ad
Michael Haschke <mhaschke@brox.de>
2017-01-31 14:47:15 +0100

    remove corrections from configuration

--- src/elements/Button/AffirmativeButton.jsx
+++ src/elements/Button/AffirmativeButton.jsx
@@ -9,29 +9,25 @@ const AffirmativeButton = React.createClass({
 
         if (__DEBUG__ && (typeof otherProps.accent !== 'undefined')) {
             console.warn('Do not use <AffirmativeButton/> with accent property.'); // eslint-disable-line no-console
-            otherProps.accent = false;
         }
 
         if (__DEBUG__ && (typeof otherProps.colored !== 'undefined')) {
             console.warn('Do not use <AffirmativeButton/> with colored property.'); // eslint-disable-line no-console
-            otherProps.colored = false;
         }
 
         if (__DEBUG__ && (typeof otherProps.dismissive !== 'undefined')) {
             console.warn('Do not use <AffirmativeButton/> with dismissive property.'); // eslint-disable-line no-console
-            otherProps.dismissive = false;
         }
 
         if (__DEBUG__ && (typeof otherProps.disruptive !== 'undefined')) {
             console.warn('Do not use <AffirmativeButton/> with disruptive property.'); // eslint-disable-line no-console
-            otherProps.disruptive = false;
         }
 
         // render button
         return (
             <Button
-                affirmative = {true}
                 {...otherProps}
+                affirmative = {true}
             >
                 {children}
             </Button>
