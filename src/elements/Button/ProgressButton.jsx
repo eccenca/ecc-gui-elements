7694e595cd71cfda972c6c1e6133a531021d63f9
Sayed Sadat <sayed.sadat@eccenca.com>
2019-03-25 10:32:42 +0100

    update docs/docTemplate.md

--- src/elements/Button/ProgressButton.jsx
+++ src/elements/Button/ProgressButton.jsx
@@ -1,5 +1,4 @@
-import React, { Component } from 'react';
-import _ from 'lodash';
+import React from 'react';
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
 import Tooltip from '../Tooltip/Tooltip';
@@ -8,17 +7,11 @@ import Progressbar from '../Progressbar/Progressbar';
 
 /**
 `<ProgressButton/>` is a special version of the `<Button/>` element that can be used to visualize a running process.
-It is shown as a raised disabled button but this behaviour can be overwritten, using the `raised` and `disabled` paramters from the `<Button/>` element.
+It is shown as a raised disabled button but this behaviour can be overwritten, using the `raised`
+ and `disabled` paramters from the `<Button/>` element.
 
 ```js
 import {ProgressButton} from '@eccenca/gui-elements';
-import rxmq from 'ecc-messagebus';
-
-// channel event which updates progressTopic
-rxmq.channel('yourchannel').subject('progressNumber').onNext({
-    progress: 30, // integer, progress in percentage
-    lastUpdate: 'August 31st 2017, 9:48:24 am.', // string which should be a date, require tooltip to be set
-});
 
 const Page = React.createClass({
     // template rendering
@@ -38,7 +31,8 @@ const Page = React.createClass({
 });
 ```
 
-You can use `progress` option directly on `<AffirmativeButton/>`, `<DismissiveButton/>` and `<DisruptiveButton/>` elements.
+You can use `progress` option directly on `<AffirmativeButton/>`, `<DismissiveButton/>`
+ and `<DisruptiveButton/>` elements.
 
 */
 const ProgressButton = props => {
@@ -90,7 +84,8 @@ ProgressButton.propTypes = {
     progress: PropTypes.number,
     /**
      message queue subject (optional): channel subject that are used to update information about progress,
-     if given that the button element listens to it for update objects that include `progressNumber` property with a value between 0 and 100
+     if given that the button element listens to it for update objects that include `progressNumber`
+     property with a value between 0 and 100
      */
     tooltip: PropTypes.string,
     /**
